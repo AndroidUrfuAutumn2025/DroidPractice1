56bf192b451bcef6ea3ab6f2161228ba99599047
Alexander Govorukhin <alexandergdev20@gmail.com>
2025-09-30 01:48:19 +0500

    Task is ready

--- app/src/main/java/ru/urfu/droidpractice1/SecondActivity.kt
+++ app/src/main/java/ru/urfu/droidpractice1/SecondActivity.kt
@@ -1,5 +1,6 @@
 package ru.urfu.droidpractice1
 
+import android.content.Intent
 import androidx.activity.ComponentActivity
 import android.os.Bundle
 import ru.urfu.droidpractice1.databinding.ActivitySecondBinding
@@ -8,12 +9,24 @@ class SecondActivity : ComponentActivity() {
 
     private lateinit var binding: ActivitySecondBinding
 
+    companion object {
+        var isArticleRead = false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySecondBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-        binding.toolbar.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
+        binding.isReadSwitch.isChecked = isArticleRead
+
+        binding.isReadSwitch.setOnCheckedChangeListener { _, isChecked ->
+            isArticleRead = isChecked
+        }
+        binding.goBackToolbar.setNavigationOnClickListener {
+            finish()
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
--- app/src/main/java/ru/urfu/droidpractice1/content/MainActivityScreen.kt
+++ app/src/main/java/ru/urfu/droidpractice1/content/MainActivityScreen.kt
@@ -2,46 +2,173 @@
 
 package ru.urfu.droidpractice1.content
 
-import androidx.compose.foundation.layout.Box
+import android.content.Context
+import android.content.Intent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ThumbUp
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.LargeTopAppBar
-import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import ru.urfu.droidpractice1.R
+import ru.urfu.droidpractice1.SecondActivity
 import ru.urfu.droidpractice1.ui.theme.DroidPractice1Theme
 
 @Composable
 fun MainActivityScreen() {
+    val context = LocalContext.current
+    val articleTitle = stringResource(R.string.main_article_title)
+    var numberOfLikes by rememberSaveable { mutableIntStateOf(0) }
+    var numberOfDislikes by rememberSaveable { mutableIntStateOf(0) }
+    val isArticleRead = SecondActivity.isArticleRead
+
     DroidPractice1Theme {
-        Scaffold(modifier = Modifier.fillMaxSize(),
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
             topBar = {
                 TopAppBar(
                     title = {
                         Text(
-                            text = stringResource(id = R.string.article_title)
+                            text = stringResource(id = R.string.header)
                         )
+                    },
+                    actions = {
+                        IconButton(
+                            onClick = {
+                                val shareIntent = Intent(Intent.ACTION_SEND).apply {
+                                    type = "text/plain"
+                                    putExtra(Intent.EXTRA_TEXT, articleTitle)
+                                }
+                                context.startActivity(Intent.createChooser(shareIntent, "Поделиться"))
+                            }) {
+                            Icon(
+                                painter = painterResource(android.R.drawable.ic_menu_share),
+                                contentDescription = "Поделиться"
+                            )
+                        }
                     }
                 )
             }) { innerPadding ->
-            Box(
-                modifier = Modifier.padding(innerPadding)
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+                    .padding(horizontal = 12.dp)
+                    .verticalScroll(rememberScrollState())
             ) {
+                Text(
+                    stringResource(R.string.main_article_title),
+                    modifier = Modifier.padding(12.dp),
+                    style = MaterialTheme.typography.titleLarge
+                )
 
+                Row(
+                    modifier = Modifier.padding(4.dp, 0.dp, 0.dp, 0.dp),
+                    horizontalArrangement = Arrangement.spacedBy(12.dp)
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        IconButton(onClick = { numberOfLikes += 1 }) {
+                            Icon(Icons.Filled.FavoriteBorder, contentDescription = "Like")
+                        }
+                        if(numberOfLikes != 0) {
+                            Text(
+                                text = numberOfLikes.toString()
+                            )
+                        }
+                    }
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        IconButton(onClick = { numberOfDislikes += 1 }) {
+                            Icon(Icons.Filled.Clear, contentDescription = "Dislike")
+                        }
+                        if(numberOfDislikes != 0) {
+                            Text(
+                                text = numberOfDislikes.toString()
+                            )
+                        }
+                    }
+                }
+
+                AsyncImage(
+                    model = R.drawable.main_article_picture,
+                    contentDescription = "null",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(12.dp)
+                        .clip(RoundedCornerShape(8.dp))
+                )
+
+                Text(
+                    stringResource(R.string.main_article_paragraph1),
+                    modifier = Modifier.padding(12.dp),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+
+                Text(
+                    stringResource(R.string.main_article_paragraph2),
+                    modifier = Modifier.padding(12.dp),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+
+                Text(
+                    stringResource(R.string.main_article_paragraph3),
+                    modifier = Modifier.padding(12.dp),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+
+                Button(
+                    onClick = {
+                        val intent = Intent(context, SecondActivity::class.java)
+                        context.startActivity(intent)
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(12.dp),
+
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = if(isArticleRead) Color.LightGray else Color.DarkGray
+                    )
+                ) {
+                    Text(stringResource(R.string.second_article_title))
+                }
             }
         }
     }
 }
-
-
 
 @Preview(showBackground = true)
 @Composable
