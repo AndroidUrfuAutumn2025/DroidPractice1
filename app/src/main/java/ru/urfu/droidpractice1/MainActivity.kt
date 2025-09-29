ec86c8731770ef3d59ae811e0a341d4c2eab729a
novchinnikov <novchinnikov@naumen.ru>
2025-09-29 22:42:09 +0500

    Done homework 2

--- app/src/main/java/ru/urfu/droidpractice1/MainActivity.kt
+++ app/src/main/java/ru/urfu/droidpractice1/MainActivity.kt
@@ -1,15 +1,49 @@
 package ru.urfu.droidpractice1
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import ru.urfu.droidpractice1.content.MainActivityScreen
 
+private const val TAG_MAIN = "MainActivityLifecycle"
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG_MAIN, "onCreate")
         setContent {
             MainActivityScreen()
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG_MAIN, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG_MAIN, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG_MAIN, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG_MAIN, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG_MAIN, "onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG_MAIN, "onRestart")
     }
 }
--- app/src/main/java/ru/urfu/droidpractice1/SecondActivity.kt
+++ app/src/main/java/ru/urfu/droidpractice1/SecondActivity.kt
@@ -1,19 +1,79 @@
 package ru.urfu.droidpractice1
 
-import androidx.activity.ComponentActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
 import ru.urfu.droidpractice1.databinding.ActivitySecondBinding
+
+private const val TAG_SECOND = "SecondActivityLifecycle"
+private const val KEY_READ_STATE = "read_state"
 
 class SecondActivity : ComponentActivity() {
 
     private lateinit var binding: ActivitySecondBinding
+    private var isRead: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG_SECOND, "onCreate")
         binding = ActivitySecondBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
 
-        binding.toolbar.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
+        isRead = intent.getBooleanExtra(KEY_READ_STATE, false)
+        binding.readSwitch.isChecked = isRead
+
+        binding.readSwitch.setOnCheckedChangeListener { _, checked ->
+            isRead = checked
+            sendResult()
+        }
+
+        binding.backButton.setOnClickListener {
+            sendResult()
+            finish()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG_SECOND, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG_SECOND, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG_SECOND, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG_SECOND, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG_SECOND, "onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG_SECOND, "onRestart")
+    }
+
+    private fun sendResult() {
+        val resultIntent = Intent().apply {
+            putExtra(KEY_READ_STATE, isRead)
+        }
+        setResult(Activity.RESULT_OK, resultIntent)
+    }
+
+    override fun onBackPressed() {
+        sendResult()
+        super.onBackPressed()
     }
 }
