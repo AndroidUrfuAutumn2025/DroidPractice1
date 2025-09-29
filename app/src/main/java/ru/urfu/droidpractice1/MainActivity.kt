f7de2680ec7272b234045d5394a193d2b469de64
Dior Yuldashov <yuldsahev.dior@mail.ru>
2025-09-30 01:53:03 +0500

    Fix build: import ArticleApp and add placeholder_image & ai_future drawables

--- app/src/main/java/ru/urfu/droidpractice1/MainActivity.kt
+++ app/src/main/java/ru/urfu/droidpractice1/MainActivity.kt
@@ -3,13 +3,18 @@ package ru.urfu.droidpractice1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import ru.urfu.droidpractice1.content.MainActivityScreen
+import androidx.compose.runtime.Composable
+import ru.urfu.droidpractice1.ui.theme.DroidPractice1Theme
+import ru.urfu.droidpractice1.ui.ArticleApp
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainActivityScreen()
+            DroidPractice1Theme {
+                ArticleApp()
+            }
         }
     }
 }
