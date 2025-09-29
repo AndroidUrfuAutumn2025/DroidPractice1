38d437eeeb78c87a1871d8db7052df41278d7e6e
GrishinaTD <nusha-grishina@mail.ru>
2025-09-29 14:22:36 +0500

    Практика "Две статьи"

--- app/src/main/java/ru/urfu/droidpractice1/MainActivity.kt
+++ app/src/main/java/ru/urfu/droidpractice1/MainActivity.kt
@@ -1,6 +1,7 @@
 package ru.urfu.droidpractice1
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import ru.urfu.droidpractice1.content.MainActivityScreen
@@ -8,8 +9,34 @@ import ru.urfu.droidpractice1.content.MainActivityScreen
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("Lifecycle", "MainActivity: onCreate")
         setContent {
             MainActivityScreen()
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("LIFECYCLE", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("LIFECYCLE", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("LIFECYCLE", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("LIFECYCLE", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("LIFECYCLE", "onDestroy")
     }
 }
--- app/src/main/java/ru/urfu/droidpractice1/SecondActivity.kt
+++ app/src/main/java/ru/urfu/droidpractice1/SecondActivity.kt
@@ -4,16 +4,56 @@ import androidx.activity.ComponentActivity
 import android.os.Bundle
 import ru.urfu.droidpractice1.databinding.ActivitySecondBinding
 
+import android.widget.TextView
+import android.widget.ImageView
+import android.widget.Switch
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+
 class SecondActivity : ComponentActivity() {
 
     private lateinit var binding: ActivitySecondBinding
+    private val PREFS_NAME = "Статьи"
+    private val KEY_READ = "статья2_прочитана"
+
+    private var switchState: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySecondBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
 
-        binding.toolbar.setNavigationOnClickListener { onBackPressedDispatcher.onBackPressed() }
+        val prefs = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        switchState = prefs.getBoolean(KEY_READ, false)
+        binding.switchBackButton.isChecked = switchState
+        binding.switchBackButton.setOnCheckedChangeListener { _, isChecked ->
+            switchState = isChecked
+            prefs.edit().putBoolean(KEY_READ, isChecked).apply()
+        }
+
+        val titleTextView = findViewById<TextView>(R.id.titleTextView)
+        val mainTextView = findViewById<TextView>(R.id.mainTextView)
+        val imageView = findViewById<ImageView>(R.id.articleImageView)
+
+
+        titleTextView.text =
+            "Начало прямой видеотрансляции мужской групповой гонки U23 на ЧМ по велоспорту"
+        mainTextView.text =
+            "26 сентября в 12:50 мск на странице Okko на «Чемпионате» начнётся прямая трансляция мужской групповой гонки U23 на чемпионате мира по велоспорту на шоссе 2025 года. Она пройдёт в Кигали (Руанда). Протяжённость маршрута составит 267,5 км."
+
+        imageView.setImageResource(R.drawable.image1)
+
+
+    }
+
+    override fun onBackPressed() {
+        val resultIntent = Intent()
+        resultIntent.putExtra("switch_state_result", switchState)
+        setResult(RESULT_OK, resultIntent)
+        super.onBackPressed()
     }
 }
+
+
+
