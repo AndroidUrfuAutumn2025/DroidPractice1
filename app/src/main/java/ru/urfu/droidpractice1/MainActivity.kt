5e61ac73e2d15abd3738c393e14997af8ec806ba
Aleks-Frank <SACHAFRANK2004@yandex.ru>
2025-09-30 07:34:47 +0500

    Frank

--- app/src/main/java/ru/urfu/droidpractice1/MainActivity.kt
+++ app/src/main/java/ru/urfu/droidpractice1/MainActivity.kt
@@ -1,8 +1,18 @@
 package ru.urfu.droidpractice1
 
+import android.content.ContentProvider
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import ru.urfu.droidpractice1.content.MainActivityScreen
 
 class MainActivity : ComponentActivity() {
@@ -12,4 +22,24 @@ class MainActivity : ComponentActivity() {
             MainActivityScreen()
         }
     }
+
+    @Composable
+    private fun MainActivityContent(){
+        Column(Modifier.fillMaxSize()){
+            Button(
+                modifier = Modifier.fillMaxSize(),
+                onClick = { }
+            ) {
+                Text(text = "Click")
+            }
+        }
+    }
+
+    @Preview
+    @Composable
+    private fun ContentProvider(){
+        MainActivityContent()
+    }
+
+
 }
