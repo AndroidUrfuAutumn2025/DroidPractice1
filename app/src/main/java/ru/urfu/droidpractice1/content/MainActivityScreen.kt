7aaa7acdbcbbecdba8dcc609eced2877db067cea
Konoplev Roman <hemp48645@gmail.com>
2026-03-27 20:31:54 +0500

    Сделал 2-ую лабу

--- app/src/main/java/ru/urfu/droidpractice1/content/MainActivityScreen.kt
+++ app/src/main/java/ru/urfu/droidpractice1/content/MainActivityScreen.kt
@@ -2,25 +2,62 @@
 
 package ru.urfu.droidpractice1.content
 
+import android.content.Intent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.filled.ThumbDown
+import androidx.compose.material.icons.filled.ThumbUp
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ru.urfu.droidpractice1.R
 import ru.urfu.droidpractice1.ui.theme.DroidPractice1Theme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import ru.urfu.droidpractice1.MainActivity
+import ru.urfu.droidpractice1.SecondActivity
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import coil.compose.AsyncImage
 
 @Composable
-fun MainActivityScreen() {
+fun MainActivityScreen(
+    likeCount: Int = 0,
+    dislikeCount: Int = 0,
+    isReadSecondArticle: Boolean = false,
+    onLikeClick: () -> Unit = {},
+    onDislikeClick: () -> Unit = {},
+    onShareClick: () -> Unit = {},
+    onReadMoreClick: () -> Unit = {}
+) {
     DroidPractice1Theme {
         Scaffold(modifier = Modifier.fillMaxSize(),
             topBar = {
@@ -29,18 +66,93 @@ fun MainActivityScreen() {
                         Text(
                             text = stringResource(id = R.string.article_title)
                         )
+                    },
+                    actions = {
+                        IconButton(onClick = onShareClick) {
+                            Icon(
+                                imageVector = Icons.Default.Share,
+                                contentDescription = stringResource(id = R.string.share_article)
+                            )
+                        }
                     }
                 )
             }) { innerPadding ->
-            Box(
-                modifier = Modifier.padding(innerPadding)
-            ) {
 
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+
+                ) {
+
+                Text(
+                    text = stringResource(R.string.first_article_title),
+                    style = MaterialTheme.typography.bodyLarge,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Spacer(Modifier.height(12.dp))
+
+                AsyncImage(
+                    model = R.drawable.first_activity_photo,
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxWidth(),
+                    contentScale = ContentScale.Fit
+                )
+                Spacer(Modifier.height(12.dp))
+
+                Text(
+                    text = stringResource(id = R.string.first_article_text),
+                    textAlign = TextAlign.Justify
+                )
+
+                Spacer(Modifier.height(12.dp))
+
+                Row(horizontalArrangement = Arrangement.spacedBy(12.dp)) {
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(4.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        IconButton(onLikeClick) {
+                            Icon(Icons.Filled.ThumbUp, contentDescription = "Like")
+                        }
+                        Text(
+                            text = likeCount.toString(),
+                            color = Color.Red
+                        )
+                    }
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(4.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        IconButton(onDislikeClick) {
+                            Icon(Icons.Filled.ThumbDown, contentDescription = "Dislike")
+                        }
+                        Text(text = dislikeCount.toString(), color = Color.Red)
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(24.dp))
+
+                Button(
+                    onClick = onReadMoreClick,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(
+                        text = if (isReadSecondArticle.not()) {
+                            stringResource(R.string.shedule_read_2_article)
+                        } else {
+                            stringResource(R.string.already_read_2_article)
+                        },
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
             }
         }
     }
 }
-
 
 
 @Preview(showBackground = true)
